4abbfd6e510bc7b807f8f6238b680c6246662452
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-03-31 09:40:54 +0530

    Java Interview

---
+++ src/starPattern/star.java
@@ -0,0 +1,45 @@
+package starPattern;
+
+public class star {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1 ;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+		
+		/*
+		*
+		**
+		***
+		****
+		*/
+		System.out.println("-------------------next program----------------------");
+		
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=4 ;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+					
+		/*
+     ****
+     ***
+     **
+     *
+		*/
+		System.out.println("-------------------next program----------------------");
+	}
+
+}

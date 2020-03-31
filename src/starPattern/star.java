4c6fd3aa588e789f4dda8d6958620fb3197d984e
Laxminarayan Jena <lnarayan.jena@gmail.com>
2020-03-31 19:58:39 +0530

    Java Interview

--- src/starPattern/star.java
+++ src/starPattern/star.java
@@ -42,6 +42,84 @@ public class star {
      *
 		*/
 		System.out.println("-------------------next program----------------------");
+	
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1 ;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=3 ;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+
+		/*
+	* 
+	**
+	***
+	****
+	***
+	**
+	*
+		 * 
+		 */
+		System.out.println("-------------------next program----------------------");
+	
+		
+		for(int i=1; i<=4;i++)
+		{
+			for(int j=4 ;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		/*
+    *
+   **
+  ***
+ ****
+		 */
+		
+System.out.println("-------------------next program----------------------");
+	
+		
+		for(int i=1; i<=4;i++)
+		{
+			for(int j=4 ;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+		
+		/*
+	 *
+    * *
+   * * *
+  * * * *
+		 */
 	}
 
+	
+	
 }

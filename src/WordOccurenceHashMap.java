d58d82d4b46bee15c64790bdf9a24814534031bc
LaxminarayanJena <lnarayan.jena@gmail.com>
2020-07-22 21:44:08 +0530

    Update WordOccurenceHashMap.java

--- src/WordOccurenceHashMap.java
+++ src/WordOccurenceHashMap.java
@@ -1,32 +1,32 @@
-
-
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-public class WordOccurenceHashMap {
+public class WordOccurence {
 
 	public static void main(String[] args) {
-
+		
 		String string = "My name is Ram Ram";
-		String[] split_str = string.split(" ");
+		String[] a = string.split(" ");
 		HashMap<String, Integer> hm = new HashMap<String, Integer>();
-		
-		for (String splited_str : split_str) {
-			if (hm.containsKey(splited_str))
-				hm.put(splited_str, hm.get(splited_str) + 1);
-			else
-				hm.put(splited_str, 1);
-		}
-		
-		
-		System.out.println(hm);
-		
-		for (Entry<String, Integer> entry : hm.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-		
-		
-		
 
+		for (int i = 0; i < a.length; i++)
+		    {
+			      if (hm.containsKey(a[i])) 
+			            {
+				         hm.put(a[i], hm.get(a[i]) + 1);
+			            } 
+			                   
+			      else 
+			      {
+				  hm.put(a[i], 1);
+			       }
+
+		    }
+		for (Entry<String, Integer> entry : hm.entrySet()) {
+
+			System.out.println(entry.getKey() + "-" + entry.getValue());
+
+		}
 	}
+
 }

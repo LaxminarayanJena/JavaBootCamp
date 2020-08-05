63fadb4f786e3372470017ea63854f0f56eb767a
LaxminarayanJena <lnarayan.jena@gmail.com>
2020-08-05 19:38:58 +0530

    Update ReverseString.java

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -5,12 +5,13 @@ public class ReverseString {
 		String s = "selenium";
 		// string is a Immutable object so no reverse function
 
-		int len = s.length();
-		String rev = "";
-		for (int i = len - 1; i >= 0; i--) {
-			rev = rev + s.charAt(i);// muineles
+		int len=s.length();
+		for(int i=len-1; i>=0;i--)
+		{	
+		System.out.print(s.charAt(i));
 		}
-		System.out.println(rev);
+		
+		
 		System.out.println("---------using buffer class-----------");
 		
 		StringBuffer sf = new StringBuffer(s);

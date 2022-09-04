9ef040a874cfdeb52cd14ec3a76cef524efc08db
LaxminarayanJena <lnarayan.jena@gmail.com>
2022-09-05 03:32:53 +0530

    Palindrome for String and Number

---
+++ src/pallindrome.java
@@ -0,0 +1,34 @@
+
+public class pallindrome {
+
+	public static void main(String[] args) {
+		int r,sum=0,temp;    
+		  int n=454;//It is the number variable to be checked for palindrome  
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;  //getting remainder  
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");   
+		  
+		  //---------------------
+		 
+		  String original = String.valueOf(n);
+		 String reverse = ""; 
+	      int length = original.length();   
+	      for ( int i = length - 1; i >= 0; i-- )  
+	         reverse = reverse + original.charAt(i);  
+	      if (original.equals(reverse))  
+	         System.out.println("Entered string/number is a palindrome.");  
+	      else  
+	         System.out.println("Entered string/number isn't a palindrome.");   
+		}  
+
+	}
+
+

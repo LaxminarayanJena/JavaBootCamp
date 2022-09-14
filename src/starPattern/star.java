296cd2c55f17f7c6bc9451df75912f23f7089082
LaxminarayanJena <lnarayan.jena@gmail.com>
2022-09-14 11:44:44 +0530

    Update star.java

--- src/starPattern/star.java
+++ src/starPattern/star.java
@@ -120,6 +120,25 @@ System.out.println("-------------------next program----------------------");
 		 */
 	}
 
+	System.out.println("-------------------next program----------------------");
 	
+		
+		int i, j, k = 1;
+            for (i = 1; i <= 5; i++) {
+                for (j = 1; j< i + 1; j++) {
+                    System.out.print(k++ + " ");
+                }
+       
+                System.out.println();
+            }
+		
+		/*
+	1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+		 */
+	}
 	
 }

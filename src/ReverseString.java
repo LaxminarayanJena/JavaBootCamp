44f943a4328fef9299106e5381f05c396b29405b
LaxminarayanJena <lnarayan.jena@gmail.com>
2023-06-20 17:27:36 +0530

    Update ReverseString.java

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -17,6 +17,20 @@ public class ReverseString {
 		StringBuffer sf = new StringBuffer(s);
 		//stringBuffer is mutable
 		System.out.println(sf.reverse());
+
+
+		System.out.println("---------using while loop-----------");
+		
+		 String str = "Hello, World!";
+	        String reversedString = "";
+               int index = str .length() - 1;
+	        
+	        while (index >= 0) {
+	            reversedString += str.charAt(index);
+	            index--;
+	        }
+	        System.out.println(reversedString);
+	    }
 	}
 
 }
